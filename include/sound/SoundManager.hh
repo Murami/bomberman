7be3ff1ea6517c2776f670ea8d967c73018e96f3
bichon_b <bichon_b@epitech.eu>
2014-06-10 18:44:11 +0200

    merge sound

---
+++ include/sound/SoundManager.hh
@@ -0,0 +1,44 @@
+
+#ifndef _SOUNDMANAGER_HH_
+# define _SOUNDMANAGER_HH_
+
+# include <map>
+# include <string>
+# include "fmod.h"
+
+namespace bbm
+{
+  class	SoundManager
+  {
+  private:
+    SoundManager();
+    ~SoundManager();
+
+  public:
+    // Load a sound or a music from a filename. If the name already exists, the new sounds erase the previous one
+    void	addMusic(std::string const& name, std::string const& path);
+    void	addSound(std::string const& name, std::string const& path);
+
+    // play a sound, if the sound doesn't exists, do nothing
+    void	play(std::string const& name);
+
+    // baisse le volume compris entre 0 et 100
+    void	volume(std::string const& name, int volume);
+    // baisse le volume général (de tous les sons)
+    void	volume(int volume);
+    void	pause(std::string const& name);
+    void	stop(std::string const& name);
+    void	del(std::string const& name);
+
+    static SoundManager*	getInstance();
+    static void			deleteInstance();
+
+  private:
+    static SoundManager*			_instance;
+    std::map<std::string, FMOD_SOUND*>		_sounds;
+    std::map<std::string, FMOD_CHANNEL*>	_channels;
+    FMOD_SYSTEM*				_system;
+  };
+};
+
+#endif /* _SOUNDMANAGER_HH_ */
