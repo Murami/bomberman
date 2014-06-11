0f2a4ee2e5b31f8428290ba43e69f93ca283dfc3
bichon_b <bichon_b@epitech.eu>
2014-06-11 15:51:58 +0200

    .

--- src/game/GameLoadingState.cpp
+++ src/game/GameLoadingState.cpp
@@ -97,7 +97,7 @@ namespace bbm
   {
     SoundManager::getInstance()->addMusic("menu", "assets/sound/Turning_tables.mp3");
     SoundManager::getInstance()->addSound("banzai", "assets/sound/banzai.mp3");
-    SoundManager::getInstance()->addSound("bomb", "assets/sound/bomb_blow.wav");
+    SoundManager::getInstance()->addSound("bomb", "assets/sound/Bomb_Blow.wav");
     SoundManager::getInstance()->addSound("aquaBomb", "assets/sound/aqua_bomb.mp3");
   }
 
