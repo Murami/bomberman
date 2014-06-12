69bab77f3fa3d45ccd388d2e43c0e2a28a14e3dc
desabr_q <desabr_q@epitech.eu>
2014-06-12 17:25:48 +0200

    opti wall

--- include/game/Wall.hh
+++ include/game/Wall.hh
@@ -1,15 +1,32 @@
 #ifndef WALL_HH
 #define WALL_HH
 
+#include "graphic/IDrawable.hh"
 #include "graphic/Object.hh"
+#include "graphic/Transformable.hh"
 
 namespace bbm
 {
-  class	Wall : public Object
+  class	ARenderer;
+  class RenderState;
+
+  class	Wall : public IDrawable, public Transformable
   {
   public:
     Wall(const std::string& texture, const std::string& shader);
     ~Wall();
+
+  public:
+    static void initialize();
+    static void release();
+
+  protected:
+    void	draw(ARenderer&, const RenderState&);
+
+  private:
+    gdl::Texture		*_texture;
+    gdl::BasicShader		*_shader;
+    static gdl::Geometry       	*_geometry;
   };
 };
 
--- src/game/Wall.cpp
+++ src/game/Wall.cpp
@@ -1,78 +1,103 @@
 #include "game/Wall.hh"
+#include "graphic/ShaderManager.hh"
+#include "graphic/TextureManager.hh"
+#include "graphic/ARenderer.hh"
+#include "graphic/RenderState.hh"
 
 namespace bbm
 {
-  Wall::Wall(const std::string& texture, const std::string& shader) :
-    Object(texture, shader, GL_QUADS)
-  {
-    //BOTTOM SIDE
-    pushVertex(glm::vec3(0, 0, 0));
-    pushVertex(glm::vec3(0, 1, 0));
-    pushVertex(glm::vec3(1, 1, 0));
-    pushVertex(glm::vec3(1, 0, 0));
+  gdl::Geometry*	Wall::_geometry = NULL;
 
-    pushUv(glm::vec2(0, 0));
-    pushUv(glm::vec2(0, 1));
-    pushUv(glm::vec2(1, 1));
-    pushUv(glm::vec2(1, 0));
+  Wall::Wall(const std::string& texture, const std::string& shader)
+  {
+    _shader = ShaderManager::getInstance()->getShader(shader);
+    _texture = TextureManager::getInstance()->getTexture(texture);
+  }
+
+  void		Wall::draw(ARenderer& renderer, const RenderState& renderState)
+  {
+    RenderState	newState = renderState;
+
+    newState.transform *= getTransform();
+    renderer.draw(*_geometry, _texture, _shader, newState, GL_QUADS);
+  }
+
+  void		Wall::release()
+  {
+    delete _geometry;
+  }
+
+  void		Wall::initialize()
+  {
+    _geometry = new gdl::Geometry();
+    //BOTTOM SIDE
+    _geometry->pushVertex(glm::vec3(0, 0, 0));
+    _geometry->pushVertex(glm::vec3(0, 1, 0));
+    _geometry->pushVertex(glm::vec3(1, 1, 0));
+    _geometry->pushVertex(glm::vec3(1, 0, 0));
+
+    _geometry->pushUv(glm::vec2(0, 0));
+    _geometry->pushUv(glm::vec2(0, 1));
+    _geometry->pushUv(glm::vec2(1, 1));
+    _geometry->pushUv(glm::vec2(1, 0));
 
     //TOP SIDE
-    pushVertex(glm::vec3(0, 0, 1));
-    pushVertex(glm::vec3(1, 0, 1));
-    pushVertex(glm::vec3(1, 1, 1));
-    pushVertex(glm::vec3(0, 1, 1));
+    _geometry->pushVertex(glm::vec3(0, 0, 1));
+    _geometry->pushVertex(glm::vec3(1, 0, 1));
+    _geometry->pushVertex(glm::vec3(1, 1, 1));
+    _geometry->pushVertex(glm::vec3(0, 1, 1));
 
-    pushUv(glm::vec2(0, 0));
-    pushUv(glm::vec2(0, 1));
-    pushUv(glm::vec2(1, 1));
-    pushUv(glm::vec2(1, 0));
+    _geometry->pushUv(glm::vec2(0, 0));
+    _geometry->pushUv(glm::vec2(0, 1));
+    _geometry->pushUv(glm::vec2(1, 1));
+    _geometry->pushUv(glm::vec2(1, 0));
 
     //NORTH SIDE
-    pushVertex(glm::vec3(0, 1, 0));
-    pushVertex(glm::vec3(0, 1, 1));
-    pushVertex(glm::vec3(1, 1, 1));
-    pushVertex(glm::vec3(1, 1, 0));
+    _geometry->pushVertex(glm::vec3(0, 1, 0));
+    _geometry->pushVertex(glm::vec3(0, 1, 1));
+    _geometry->pushVertex(glm::vec3(1, 1, 1));
+    _geometry->pushVertex(glm::vec3(1, 1, 0));
 
-    pushUv(glm::vec2(0, 0));
-    pushUv(glm::vec2(0, 1));
-    pushUv(glm::vec2(1, 1));
-    pushUv(glm::vec2(1, 0));
+    _geometry->pushUv(glm::vec2(0, 0));
+    _geometry->pushUv(glm::vec2(0, 1));
+    _geometry->pushUv(glm::vec2(1, 1));
+    _geometry->pushUv(glm::vec2(1, 0));
 
     //SOUTH SIDE
-    pushVertex(glm::vec3(0, 0, 0));
-    pushVertex(glm::vec3(1, 0, 0));
-    pushVertex(glm::vec3(1, 0, 1));
-    pushVertex(glm::vec3(0, 0, 1));
+    _geometry->pushVertex(glm::vec3(0, 0, 0));
+    _geometry->pushVertex(glm::vec3(1, 0, 0));
+    _geometry->pushVertex(glm::vec3(1, 0, 1));
+    _geometry->pushVertex(glm::vec3(0, 0, 1));
 
-    pushUv(glm::vec2(0, 0));
-    pushUv(glm::vec2(0, 1));
-    pushUv(glm::vec2(1, 1));
-    pushUv(glm::vec2(1, 0));
+    _geometry->pushUv(glm::vec2(0, 0));
+    _geometry->pushUv(glm::vec2(0, 1));
+    _geometry->pushUv(glm::vec2(1, 1));
+    _geometry->pushUv(glm::vec2(1, 0));
 
     //EAST SIDE
-    pushVertex(glm::vec3(1, 0, 0));
-    pushVertex(glm::vec3(1, 1, 0));
-    pushVertex(glm::vec3(1, 1, 1));
-    pushVertex(glm::vec3(1, 0, 1));
+    _geometry->pushVertex(glm::vec3(1, 0, 0));
+    _geometry->pushVertex(glm::vec3(1, 1, 0));
+    _geometry->pushVertex(glm::vec3(1, 1, 1));
+    _geometry->pushVertex(glm::vec3(1, 0, 1));
 
-    pushUv(glm::vec2(0, 0));
-    pushUv(glm::vec2(0, 1));
-    pushUv(glm::vec2(1, 1));
-    pushUv(glm::vec2(1, 0));
+    _geometry->pushUv(glm::vec2(0, 0));
+    _geometry->pushUv(glm::vec2(0, 1));
+    _geometry->pushUv(glm::vec2(1, 1));
+    _geometry->pushUv(glm::vec2(1, 0));
 
     //WEST SIDE
-    pushVertex(glm::vec3(0, 0, 0));
-    pushVertex(glm::vec3(0, 0, 1));
-    pushVertex(glm::vec3(0, 1, 1));
-    pushVertex(glm::vec3(0, 1, 0));
+    _geometry->pushVertex(glm::vec3(0, 0, 0));
+    _geometry->pushVertex(glm::vec3(0, 0, 1));
+    _geometry->pushVertex(glm::vec3(0, 1, 1));
+    _geometry->pushVertex(glm::vec3(0, 1, 0));
 
-    pushUv(glm::vec2(0, 0));
-    pushUv(glm::vec2(0, 1));
-    pushUv(glm::vec2(1, 1));
-    pushUv(glm::vec2(1, 0));
+    _geometry->pushUv(glm::vec2(0, 0));
+    _geometry->pushUv(glm::vec2(0, 1));
+    _geometry->pushUv(glm::vec2(1, 1));
+    _geometry->pushUv(glm::vec2(1, 0));
 
-    setColor(glm::vec4(1, 1, 1, 1));
-    build();
+    _geometry->setColor(glm::vec4(1, 1, 1, 1));
+    _geometry->build();
   }
 
   Wall::~Wall()
